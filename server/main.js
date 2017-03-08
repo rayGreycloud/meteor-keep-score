a1d7d2a59c44de9e6a63f580c95e75e035c53cb2
rayGreycloud <rayGreycloud@gmail.com>
2017-03-08 09:19:28 -0800

    Refactor client main using arrow functions

--- server/main.js
+++ server/main.js
@@ -1,11 +1,4 @@
 import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
-Meteor.startup(function () {
-  // Players.insert({
-  //   name: 'Darth',
-  //   score: '-1'
-  // });
-
-  console.log(Players.find().fetch());
-});
+Meteor.startup(() => console.log('Log from /server/main.js'));
