16f068120432e74c70dfb504a85980c0883c39a6
rayGreycloud <rayGreycloud@gmail.com>
2017-03-09 11:43:18 -0800

    Add scss support and style classes to TitleBar

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -11,9 +11,11 @@ class TitleBar extends React.Component {
   render() {
 
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        {this.renderSubtitle()}
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{this.props.title}</h1>
+          {this.renderSubtitle()}
+        </div>
       </div>
     );
   }
