64d41372c6cdef3c955785b842b68c59f4609254
rayGreycloud <rayGreycloud@gmail.com>
2017-03-11 07:10:27 -0800

    Destructure props in Player render method

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,11 +4,7 @@ import { Players } from './../api/players';
 
 class Player extends Component {
   render() {
-    let _id = this.props.player._id;
-    let name = this.props.player.name;
-    let score = this.props.player.score;
-    let rank = this.props.player.rank;
-    let position = this.props.player.position;
+    let {_id, name, score, rank, position} = this.props.player;
 
     return (
       <div key={_id} className="item">
